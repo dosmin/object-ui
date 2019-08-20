30470704587a08a192c1452a7ea53d62eba02293
Michelle <chellicmiller@gmail.com>
2019-08-19 23:41:22 -0500

    #1 Add JSDocs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/**
+ * Simple function to act as a placeholder.
+ * 
+ * @param {string} name 
+ */
 export function hello(name) {
     console.log(`Hello, ${name}!`);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
 
+/**
+ * Basic configuration for webpack.
+ * 
+ * Currently does not support styles, JSX files, or images.
+ */
 const config = {
   entry: './src/index.js',
   output: {
